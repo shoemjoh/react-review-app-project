43740f09ed2859e43a7e6eef3de5dccd8f591cf9
John Shoemaker <shoemjoh@gmail.com>
2024-09-16 22:38:05 -0500

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div>
-            Nav Bar Test
-        </div>
+        <nav>
+            <div>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/destinations">Destinations</Link></li>
+                <li><Link to="/hotels">Hotels</Link></li>
+                <li><Link to="/restaurants">Restaurants</Link></li>
+            </div>
+        </nav>
+
     )
 }
 
---
+++ src/components/ReviewList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const ReviewList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ReviewList
