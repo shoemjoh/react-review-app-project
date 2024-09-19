f9f8277c9d4785ae3211e2b89b68ceb132e25a63
John Shoemaker <shoemjoh@gmail.com>
2024-09-18 22:55:34 -0500

    added route for Destination Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import Destinations from './pages/Destinations';
+import DestinationPage from './pages/DestinationPage';
 import Hotels from './pages/Hotels';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Restaurants from './pages/Restaurants';
@@ -23,6 +24,7 @@ function App() {
           <Route path="/destinations" element={<Destinations />} />
           <Route path="/hotels" element={<Hotels />} />
           <Route path="/restaurants" element={<Restaurants />} />
+          <Route path="/destinations/:destination" element={<DestinationPage />} />
         </Routes>
       </div>
     </Router>
