02a6204e290ed96203ba783645726e1afb0076cc
John Shoemaker <shoemjoh@gmail.com>
2024-09-22 23:21:24 -0400

    form css styling implemented

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -40,42 +40,44 @@ const ReviewForm = ({ addReview }) => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Destination (City):
-                <input
-                    type="text"
-                    name="destination"
-                    value={formData.destination}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Category:
-                <select name="category" value={formData.category} onChange={handleChange}>
-                    <option value="Hotel">Hotel</option>
-                    <option value="Restaurant">Restaurant</option>
-                </select>
-            </label>
-            <label>
-                Review:
-                <textarea
-                    name="review"
-                    value={formData.review}
-                    onChange={handleChange}
-                />
-            </label>
-            <button type="submit">Submit Review</button>
-        </form>
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Destination (City):
+                    <input
+                        type="text"
+                        name="destination"
+                        value={formData.destination}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Category:
+                    <select name="category" value={formData.category} onChange={handleChange}>
+                        <option value="Hotel">Hotel</option>
+                        <option value="Restaurant">Restaurant</option>
+                    </select>
+                </label>
+                <label>
+                    Review:
+                    <textarea
+                        name="review"
+                        value={formData.review}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">Submit Review</button>
+            </form>
+        </div>
     )
 }
 
