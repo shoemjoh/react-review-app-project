7e5597ce89ae338b4186763c69a41cb47030b638
John Shoemaker <shoemjoh@gmail.com>
2024-09-22 22:11:07 -0400

    use effect working on destinations page

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReviewList from '../components/ReviewList'
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const Destinations = () => {
+
+    useEffect(() => {
+        console.log('Use Effect hook working')
+    }, [])
+
+    // Render a page with a list of Destinations that have been reviewed.
+    // Want to fetch the list of destinations from the json database when the page mounts. useEffect and useState?
 
 
     return (
