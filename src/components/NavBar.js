cb4a685066bc6fa43419bc5af4539cf81507890e
John Shoemaker <shoemjoh@gmail.com>
2024-09-22 23:14:01 -0400

    formatted nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav>
-            <div>
+        <nav className="navbar">
+            <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/destinations">Destinations</Link></li>
                 <li><Link to="/hotels">Hotels</Link></li>
                 <li><Link to="/restaurants">Restaurants</Link></li>
-            </div>
+            </ul>
         </nav>
-
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
+
