20715384c666b290679cc4c32d724a15699ccef9
John Shoemaker <shoemjoh@gmail.com>
2024-09-22 22:26:31 -0400

    fetching data from db json and creating a unique destinations array

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReviewList from '../components/ReviewList'
 
 const Destinations = () => {
+    const [destinations, setDestinations] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:4000/reviews').
             then(res => res.json()).
-            then(data => console.log("Fetch working"))
+            then(data => (data) => {
+                const uniqueDestinations = [...new Set(data.map((review) => review.destination))];
+                setDestinations(uniqueDestinations);
+            })
     }, [])
 
     // Render a page with a list of Destinations that have been reviewed.
-    // Want to fetch the list of destinations from the json database when the page mounts. useEffect and useState?
+    // Want to fetch the list of destinations from the json database when the page mounts (useEffect)
+    // Map through each destination and render it to the page (this has already been done with destination tiles)
 
 
     return (
